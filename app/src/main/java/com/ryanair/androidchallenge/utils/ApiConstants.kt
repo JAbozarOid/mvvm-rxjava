3ea8ae327ae0d9ca0eb3a1db9ba52e2fbf7882a0
a.raghibdoost <myrad.raghibdoost@gmail.com>
2022-05-28 09:37:19 +0430

    api_constants_added

---
+++ app/src/main/java/com/ryanair/androidchallenge/utils/ApiConstants.kt
@@ -0,0 +1,13 @@
+package com.ryanair.androidchallenge.utils
+
+class ApiConstants {
+
+    companion object {
+
+        // base url for AirportService and RoutesService
+        const val BASE_URL_SERVICES_API = "https://services-api.ryanair.com/"
+
+        // base url for flight services
+        const val BASE_URL_FLIGHTS = "https://www.ryanair.com/api/"
+    }
+}
